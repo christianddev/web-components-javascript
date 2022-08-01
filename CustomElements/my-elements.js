d28f0037aa00817f31954c90b106f995bde0955a
Christian David G <cdavid.ggarcia@gmail.com>
2022-08-01 07:03:48 +0200

    feat: apply styles using css custom properties

--- CustomElements/my-elements.js
+++ CustomElements/my-elements.js
@@ -20,6 +20,10 @@ class MyElement extends HTMLElement {
     return /*html*/ `
       <style>
         :host {
+          --primary-color: tomato;
+          --secondary-color: orange;
+          --heading-primary: 50px;
+          --heading-secondary: 30px;
           display: inline-block;
           width: 100%;
           min-width: 300px;
@@ -44,6 +48,18 @@ class MyElement extends HTMLElement {
         ::slotted(.color-green) {
           color: green;
         }
+        section {
+          background-color: var(--primary-color);
+        }
+        section div {
+          background-color: var(--secondary-color);
+        }
+        h1 {
+          font-size: var(--heading-primary);
+        }
+        p {
+          font-size: var(--heading-secondary);
+        }
       </style>
     `;
   }
@@ -53,11 +69,11 @@ class MyElement extends HTMLElement {
     <section>
       <h1 ><slot name="title"></slot></h1>
       <h2 class="subtitle">${this?.subtitle}</h2>
-        <div>
-          <p><slot name="paragraph"></slot></p>
-          <p>${this?.otherparagraph}</p>
-          <img src="${this.imageurl ?? ''}" alt="${this?.imagedescription ?? ''}" />
-        </div>
+      <div>
+        <p><slot name="paragraph"></slot></p>
+        <p>${this?.otherparagraph}</p>
+        <img src="${this.imageurl ?? ''}" alt="${this?.imagedescription ?? ''}" />
+      </div>
     </section>
     ${this.getStyles()}
     `;
